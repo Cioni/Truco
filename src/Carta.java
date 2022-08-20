a29d0929ba62f47621ad2cbc9b8fd9d39ffb8aba
nachin <nachii.cb.97@hotmail.com>
2022-08-20 12:10:25 -0300

    Initial commit

---
+++ src/Carta.java
@@ -0,0 +1,16 @@
+public class Carta {
+    private Palo palo;
+    private int valor;
+
+    public Carta(Palo p, int v){
+        palo = p;
+        valor = v;
+    }
+
+    public static CartaVersus comparar(Carta a, Carta b){
+
+    }
+
+}
+
+
